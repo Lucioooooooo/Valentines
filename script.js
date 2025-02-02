f4d2b6b8a5ad26f4419bad4ea30fe951b8e3c874
Lucioooooooo <donlucioandal@gmail.com>
2025-02-02 15:43:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function moveRandoEl(elm) {
+    elm.style.position = "abosolute";
+    elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+    elm.style.left= Math.floor(Math.random() * 90 + 5) + "%";
+}
+
+const moveRandom = document.querySelector("#move-random");
+
+moveRandom.addEventListener("mouseenter", function(e) {
+    moveRandomEl(e.target);
+});
