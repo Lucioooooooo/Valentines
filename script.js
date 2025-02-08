60fc7b945834c04e23ffc89b5e03f0cc193888ce
Lucioooooooo <donlucioandal@gmail.com>
2025-02-08 23:10:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function moveRandoEl(elm) {
     elm.style.position = "absolute";
     elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
-     elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+    elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
 }
 
 const moveRandom = document.querySelector("#move-random");
 
-moveRandom.EventListener("mouseenter", function(e) {
-    moveRandomEl(e.target);
+moveRandom.addEventListener("click", function(e) {
+    moveRandoEl(e.target);
 });
